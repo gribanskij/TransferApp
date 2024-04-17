987339edaa037650b10653e949c31e465ee0ab3d
Dmitriy Gribanskij <gribanskij1979@gmail.com>
2024-04-17 17:25:56 +0300

    eee

--- TransferApp/SecondViewController.swift
+++ TransferApp/SecondViewController.swift
@@ -1,21 +1,21 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  TransferApp
 //
 //  Created by Дмитрий Грибанский on 16.04.2024.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+class SecondViewController : UIViewController {
     
-    @IBOutlet var dataLabel: UILabel!
-
+    @IBOutlet var dataTextField: UITextField?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
-
